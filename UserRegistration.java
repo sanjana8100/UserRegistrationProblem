f48587de3e56936ad8a0e83411f24db99e0f6a36
sanjana8100 <sanjanams812@gmail.com>
2023-03-28 23:30:06 +0530

    added: UC5

--- UserRegistration.java
+++ UserRegistration.java
@@ -45,5 +45,12 @@ public class UserRegistration {
         Pattern phoneNoPattern = Pattern.compile("^[0-9]{2}[0-9]{10}$");
         Matcher phoneNoMatcher = phoneNoPattern.matcher(phoneNumber);
         isValid(phoneNoMatcher);
+
+        System.out.print("Enter Password: ");
+        String password = in.next();
+
+        Pattern passwordPattern = Pattern.compile("^[a-zA-Z0-9]{8}$");
+        Matcher passwordMatcher = passwordPattern.matcher(password);
+        isValid(passwordMatcher);
     }
 }
