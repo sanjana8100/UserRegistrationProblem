5cddf71c7fc20db942a039fb4ca09152ab18ae38
sanjana8100 <sanjanams812@gmail.com>
2023-03-28 19:59:29 +0530

    added: UC1

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,7 +1,26 @@
 package com.bridgelabz.UserRegistrationProblem;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
     public static void main(String[] args) {
-        System.out.println("User Registration Problem");
+        System.out.println("----------User Registration----------");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String firstName = sc.next();
+
+        Pattern namePattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Matcher firstNameMatcher = namePattern.matcher(firstName);
+
+        if (firstNameMatcher.matches()) {
+            System.out.print("Valid");
+        }
+        else {
+            System.out.print("Invalid");
+        }
     }
 }
