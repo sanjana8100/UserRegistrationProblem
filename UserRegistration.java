509ac8c747fdf82db49e80e5c629d57a47a13ff5
sanjana8100 <sanjanams812@gmail.com>
2023-03-28 19:54:03 +0530

    added: User Registration Problem

---
+++ UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.UserRegistrationProblem;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("User Registration Problem");
+    }
+}
