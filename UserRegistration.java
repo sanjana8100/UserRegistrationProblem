06e7f2c6c33429c05011113b1b7d881155652427
sanjana8100 <sanjanams812@gmail.com>
2023-03-28 20:07:41 +0530

    added: UC2

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,6 +5,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+    public static void isValid(Matcher matcher){
+        if (matcher.matches()) {
+            System.out.println("---Valid---");
+        }
+        else {
+            System.out.println("--Invalid--");
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("----------User Registration----------");
 
@@ -13,14 +22,14 @@ public class UserRegistration {
         System.out.print("Enter First Name: ");
         String firstName = sc.next();
 
-        Pattern namePattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Pattern namePattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
         Matcher firstNameMatcher = namePattern.matcher(firstName);
+        isValid(firstNameMatcher);
 
-        if (firstNameMatcher.matches()) {
-            System.out.print("Valid");
-        }
-        else {
-            System.out.print("Invalid");
-        }
+        System.out.print("\nEnter Last Name: ");
+        String lastName = sc.next();
+
+        Matcher lastNameMatcher = namePattern.matcher(lastName);
+        isValid(lastNameMatcher);
     }
 }
